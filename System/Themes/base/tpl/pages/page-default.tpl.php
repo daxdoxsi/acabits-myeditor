5776a0abd3e015147f72541d6f6994bfc9cb615a
David M. Miller <daxdoxsi@gmail.com>
2022-04-24 04:45:07 -0600

    Splitting the main page in components

--- System/Themes/base/tpl/pages/page-default.tpl.php
+++ System/Themes/base/tpl/pages/page-default.tpl.php
@@ -35,14 +35,13 @@ use Acabits\System\Template;
             <!-- Content Wrapper START -->
             <div class="main-content">
                 <div class="page-header">
-                    <h2 class="header-title">Title</h2>
+                    <h2 class="header-title">Welcome to Acabits App Builder</h2>
                     <div class="header-sub-title">
                         <nav class="breadcrumb breadcrumb-dash">
                             <a href="#" class="breadcrumb-item"><i class="anticon anticon-home m-r-5"></i>Home</a>
                             <a class="breadcrumb-item" href="#">Breadcrumb 1</a>
                             <span class="breadcrumb-item active">Breadcrumb 2</span>
                         </nav>
-                        <h1>Welcome to Acabits</h1>
                     </div>
                 </div>
                 <!-- Content goes Here -->
